715256ada2787eebe05a161fad272a361439f514
yuyang339 <109053818+yangyu3390@users.noreply.github.com>
2023-12-25 10:20:02 +1100

    4.4

---
+++ lox/Scanner.java
@@ -0,0 +1,35 @@
+package com.craftinginterpreters.lox;
+
+import java.util.ArrayList;
+import java.util.List;
+import jave.util.HashMap;
+import jave.util.Map;
+import static com.craftinginterpreters.lox.TokenType.*;
+
+public class Scanner {
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
+    Scanner(String source) {
+        this.source = source;
+    }
+
+    List<Token> scanTokens() {
+        while (!isAtEnd()) {
+            start = current;
+            scanToken();
+        }
+        tokens.add(new Token(EOF, "", null, line));
+        return tokens;
+    }
+
+    private boolean isAtEnd() {
+        return current >= source.length();
+    }
+
+    private void scanToken() {
+
+    }
+}
